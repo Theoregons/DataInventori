eb279b622dd8191f5b1f6a2d71b4adfc80e43dde
Baktiar Mahardika <baktiarmahardika08@gmail.com>
2024-06-26 15:32:45 +0700

    db

--- inventori.sql
+++ inventori.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 31, 2024 at 04:05 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Waktu pembuatan: 26 Jun 2024 pada 10.29
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,16 +18,17 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `toko`
+-- Database: `inventori`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `barang`
+-- Struktur dari tabel `barang`
 --
 
 CREATE TABLE `barang` (
+  `id` int(11) NOT NULL,
   `kode` varchar(10) NOT NULL,
   `nama` varchar(16) NOT NULL,
   `jumlah` int(4) NOT NULL,
@@ -36,16 +37,17 @@ CREATE TABLE `barang` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `barang`
+-- Dumping data untuk tabel `barang`
 --
 
-INSERT INTO `barang` (`kode`, `nama`, `jumlah`, `harga`, `merek`) VALUES
-('001', 'Sabun Cuci', 2, 4000, 'GIV');
+INSERT INTO `barang` (`id`, `kode`, `nama`, `jumlah`, `harga`, `merek`) VALUES
+(1, '001', 'Sabun Cuci', 22, 120000, 'Cussons'),
+(2, '002', 'Tissue', 2, 20000, 'Pad');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `pegawai`
+-- Struktur dari tabel `pegawai`
 --
 
 CREATE TABLE `pegawai` (
@@ -59,22 +61,40 @@ CREATE TABLE `pegawai` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `pegawai`
+-- Dumping data untuk tabel `pegawai`
 --
 
 INSERT INTO `pegawai` (`kodepeg`, `namapeg`, `alamat`, `jkel`, `jabatan`, `username`, `password`) VALUES
-('24.05.31.001', 'Khairul Anwar', 'Mataram', 'L', 'P', 'iroelmaiden', 'kayangan118');
+('001', 'Jocelyn', 'Jakarta Selatan', 'L', 'P', 'Joce', '12345678'),
+('003', 'Gilang', 'Jalan Terus', 'L', 'P', 'gilang', '12345678'),
+('004', 'Ilham', 'Jalan Besar', 'L', 'P', 'ilham', '12345678');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `pegawai`
+-- Indeks untuk tabel `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `pegawai`
 --
 ALTER TABLE `pegawai`
   ADD PRIMARY KEY (`kodepeg`),
   ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `barang`
+--
+ALTER TABLE `barang`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
