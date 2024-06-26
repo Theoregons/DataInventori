6d58ad4aca354f4fc350a379c64c38d4ccde356d
Baktiar Mahardika <baktiarmahardika08@gmail.com>
2024-06-26 15:28:38 +0700

    cari pegawai

---
+++ src/controller/controllerMenuUtama.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+import view.FormBarang;
+import view.FormLupa;
+import view.FormPegawai;
+import view.MenuUtama;
+import view.FormCariPegawai;
+
+
+/**
+ *
+ * @author Bakti
+ */
+public class controllerMenuUtama {
+    MenuUtama frame;
+    FormBarang fBarang;
+    FormPegawai fPegawai;
+    FormLupa fLupaPass;
+    FormCariPegawai fCariPegawai;
+    
+    public controllerMenuUtama(MenuUtama frame) {
+        this.frame = frame;
+    }
+    
+    public void masukFormBarang() {
+        fBarang = new FormBarang();
+        fBarang.setVisible(true);
+    }
+   
+    public void masukFormPegawai() {
+        fPegawai = new FormPegawai();
+        fPegawai.setVisible(true);
+    }
+    
+    public void masukFormLupaPass() {
+        fLupaPass = new FormLupa();
+        fLupaPass.setVisible(true);
+    }
+    
+    
+    public void masukCariPegawai() {
+        fCariPegawai = new FormCariPegawai();
+        fCariPegawai.setVisible(true);
+    }
+    
+    public void keluar() {
+        frame.dispose();
+    }
+}
